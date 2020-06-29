0ff924427b88c4c9361eff4159acb639189d2150
BenRodkin <54512462+BenRodkin@users.noreply.github.com>
2020-06-29 14:41:55 -0400

    Put first maze in its own folder

--- bot.js
+++ bot.js
@@ -247,7 +247,7 @@ try{
 //}
 
 function sendLocation(message) {
-    message.channel.send(new Discord.MessageAttachment("./images/maze/x"+xPos+"y"+yPos+".png"))
+    message.channel.send(new Discord.MessageAttachment("./images/mazes/0/x"+xPos+"y"+yPos+".png"))
     message.channel.send("Type '"+PREFIX+"Up', '"+PREFIX+"down', '"+PREFIX+"left', and '"+PREFIX+"right' to move around. Type '"+PREFIX+"resetmaze' to reset the maze.")
 }
 function cantMove(message) {
